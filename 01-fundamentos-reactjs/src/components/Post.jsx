f210b2b10a53656798d3649aa64392d18f17724b
adhmattheus <mattheusadhonnaysf@hotmail.com>
2023-05-04 18:14:37 -0300

    formlario de comentarios

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -27,33 +27,20 @@ export function Post() {
 					</p>
 				</div>
 
-			</article>
+				<form className={styles.commentForm}>
+					<strong>Deixe seu feedback</strong>
+					<textarea
+						placeholder='Deixe um comentário'
+					/>
 
-			<article className={styles.post}>
-				<header>
-					<div className={styles.author}>
-						<img className={styles.avatar} src="https://avatars.githubusercontent.com/u/30497608?v=4" />
-						<div className={styles.authorInfo}>
-							<strong>Mattheus Adhonnay</strong>
-							<span>Web Developer</span>
-						</div>
-					</div>
+					<footer>
+						<button type='submit'>Publicar</button>
+					</footer>
 
-					<time title='04 de Maio às 09:20:30' dateTime='2023-04-05 09:20:30'>Publicado há 1h</time>
-				</header>
-
-				<div className={styles.content}>
-					<p>	Fala galeraa 👋	</p>
-					<p>	Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-					<p>👉 <a href="#">jane.design/doctorcare</a></p>
-					<p>
-						<a href="">#novoprojeto</a>
-						<a href=""> #nlw </a>
-						<a href="">#rocketseat</a>
-					</p>
-				</div>
+				</form>
 
 			</article>
+
 		</>
 	);
 } 
