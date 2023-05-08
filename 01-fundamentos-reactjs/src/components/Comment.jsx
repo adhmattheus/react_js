db4950372fe5d22a6d3fec0747069b9708de2ce7
adhmattheus <mattheusadhonnaysf@hotmail.com>
2023-05-08 17:49:41 -0300

    funcao para likes nos comentarios

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,11 +1,18 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment({ content, onDeletComment }) {
 
+	const [likeCount, setLikeCount] = useState(0);
+
 	function handleDeletComment() {
 		onDeletComment(content);
+	}
+
+	function handleLikeComment() {
+		setLikeCount(likeCount + 1);
 	}
 
 	return (
@@ -33,9 +40,9 @@ export function Comment({ content, onDeletComment }) {
 				</div>
 
 				<footer>
-					<button>
+					<button onClick={handleLikeComment}>
 						<ThumbsUp />
-						Aplaudir <span>20</span>
+						Aplaudir <span>{likeCount}</span>
 					</button>
 				</footer>
 			</div>
