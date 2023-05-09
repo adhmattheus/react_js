885ad39e87459b738beeec1fd1cd2e6cf2d40f0a
adhmattheus <mattheusadhonnaysf@hotmail.com>
2023-05-09 13:28:14 -0300

    adicionando typescript np app

---
+++ 01-fundamentos-react-ts/src/components/Comment.tsx
@@ -0,0 +1,58 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+import { Avatar } from './Avatar';
+import { useState } from 'react';
+
+interface CommentProps {
+	content: string,
+	onDeletComment: (comment: string) => void,
+}
+
+export function Comment({ content, onDeletComment }: CommentProps) {
+
+	const [likeCount, setLikeCount] = useState(0);
+
+	function handleDeletComment() {
+		onDeletComment(content);
+	}
+
+	function handleLikeComment() {
+		setLikeCount((state) => {
+			return state + 1
+		});
+	}
+
+	return (
+		<div className={styles.comment}>
+
+			<Avatar hasBorder={false} src="https://github.com/maykbrito.png" alt='' />
+
+			<div className={styles.commentBox}>
+
+				<div className={styles.commentContent}>
+					<header>
+						<div className={styles.authorAndTime}>
+							<strong>Mayk Brito</strong>
+							<time title='04 de Maio às 09:20:30' dateTime='2023-04-05 09:20:30'>Cerca de 1h atrás</time>
+						</div>
+
+						<button
+							onClick={handleDeletComment}
+							title='Deletar comentário'>
+							<Trash size={24} />
+						</button>
+					</header>
+
+					<p>{content}</p>
+				</div>
+
+				<footer>
+					<button onClick={handleLikeComment}>
+						<ThumbsUp />
+						Aplaudir <span>{likeCount}</span>
+					</button>
+				</footer>
+			</div>
+		</div>
+	);
+}
---
+++ 01-fundamentos-react-ts/src/components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { PencilLine } from 'phosphor-react';
+import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
+
+
+export function Sidebar() {
+	return (
+		<aside className={styles.sidebar}>
+			<img
+				className={styles.cover}
+				src="https://images.unsplash.com/photo-1629904853893-c2c8981a1dc5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="" />
+
+			<div className={styles.profile}>
+				<Avatar  src="https://github.com/adhmattheus.png" />
+				<strong>Mattheus Adhonnay</strong>
+				<span>Web Developer</span>
+			</div>
+
+			<footer>
+				<a href="#">
+					<PencilLine size={20} />
+					Editar seu perfil
+				</a>
+			</footer>
+		</aside>
+	);
+}
